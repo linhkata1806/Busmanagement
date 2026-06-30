ef2068edfc736c4fb7b16fcf411c87d63c3796c3
naropham <phamhuyen12a1@gmail.com>
2026-06-30 10:18:37 +0700

    remove api weather and save in package api

--- busmanagement/src/java/controller/WeatherServlet.java
+++ busmanagement/src/java/controller/WeatherServlet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import service.WeatherService;
+import api.WeatherService;
 
 @WebServlet(name = "WeatherServlet", urlPatterns = {"/weather"})
 public class WeatherServlet extends HttpServlet {
